fb726bc8da785a0c3aaaf3735d848538c9a635e7
Dim <dimitri.kiavue@gmail.com>
2018-07-26 14:17:24 +0200

    Create linked Tables and Faker Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+
+10.times do
+  doctor = Doctor.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, speciality: Faker::Beer.style, postal_code: Faker::Number.between(75000, 95999))
+end
+
+20.times do
+  patient = Patient.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
+
+30.times do
+  appointment = Appointment.create(date: Faker::Date.forward(38), doctor_id: Faker::Number.between(1, 10), patient_id: Faker::Number.between(1, 20))
+end

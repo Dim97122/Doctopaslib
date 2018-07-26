2ff67a35879075e90b2649191aec49c288e490b6
Dim <dimitri.kiavue@gmail.com>
2018-07-26 18:33:02 +0200

    Doctor Pimp

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require 'faker'
 
 
 10.times do
-  doctor = Doctor.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, speciality: Faker::Beer.style, postal_code: Faker::Number.between(75000, 95999))
+  doctor = Doctor.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, postal_code: Faker::Number.between(75000, 95999))
 end
 
 20.times do
@@ -11,5 +11,20 @@ end
 end
 
 30.times do
-  appointment = Appointment.create(date: Faker::Date.forward(38), doctor_id: Faker::Number.between(1, 10), patient_id: Faker::Number.between(1, 20))
+  appointment = Appointment.create(date: Faker::Date.forward(38), doctor_id: Faker::Number.between(1, 20), patient_id: Faker::Number.between(1, 40))
+end
+
+10.times do
+  speciality = Speciality.create(speciality_name: Faker::Food.dish)
+end
+
+Speciality.all.each do |speciality|
+  speciality.doctors.push(Doctor.all.sample)
+  speciality.save
+end
+
+# give each assembly a random part
+Doctor.all.each do |doctor|
+  doctor.specialities.push(Speciality.all.sample)
+  doctor.save
 end
